7b2e9069371606d2eedb96f646332abb3503b232
syifnrl <syifnrl@localhost>
2022-05-15 14:28:29 +0000

    Update with example;

--- RotaryEncoder.h
+++ RotaryEncoder.h
@@ -2,12 +2,63 @@
 #define MBED_ROTARYENCODER_H
  
 #include "mbed.h"
+/*
+    A library for quadrate incremental encoder. There are three encoding
+    that can be used to determine posistion in quadrate incremental encoder; X1, X2, X4. For more information,
+    you can see in: https://www.motioncontroltips.com/faq-what-do-x1-x2-and-x4-position-encoding-mean-for-incremental-encoders/
+    
+    Example on using X4 encoding: 
+    #include "mbed.h"
+    #include "RotaryEncoder.h"
+    
+    RotaryEncoder pA(PC_6, PC_5, 4);//
+    Serial pc(USBTX, USBRX);
+    
+    int main(){
+        while(1){
+         pc.printf("%d\n", pA.getPulse());       
+        }
+    }
+    
+    ------------------------------------------------------------------------------------------------------------------------------
+    
+    Library untuk quadrate incremental encoder. Terdapat tiga jenis encoding
+    yang dapat dipakai dalam menentukan posisi pada quadrate incremental encoder.
+    Untuk informasi lebih lanjut, dapat dilihat pada: 
+    https://www.motioncontroltips.com/faq-what-do-x1-x2-and-x4-position-encoding-mean-for-incremental-encoders/
+    
+    Contoh code untuk penggunaan encoding X4:
+    #include "mbed.h"
+    #include "RotaryEncoder.h"
+    
+    RotaryEncoder pA(PC_6, PC_5, 4);//
+    Serial pc(USBTX, USBRX);
+    
+    int main(){
+        while(1){
+         pc.printf("%d\n", pA.getPulse());       
+        }
+    }
+    
+
+*/
 
 class RotaryEncoder{
 public:
+    //inisialisasi
     RotaryEncoder(PinName cA, PinName cB, int mode=1);
+    
+    //Ambil posisi encoder
     int getPulse();
+    
+    /*ambil kecepatan dari encoder:
+        x(t) = ∆x //perpindahan posisi  ->    x = pulse(p)     p(t) = ∆p
+        v(t) = ∆x/∆t //kecepatan        ->    v(t) = ∆p/∆t
+             = x'(t) 
+    */  
     float getFreq();
+    
+    //Ambil mode encoding
     int getEncoding(){return mode;}
     private:
     InterruptIn cA;
